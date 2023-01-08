ffba28eecf5e0dee6146a38b677beb248d8b9d90
Sannasi <sannasichottu@gmail.com>
2023-01-09 01:08:12 +0530

    cors added

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 require("dotenv").config();
+const cors = require('cors')
 
-
+app.use(cors());
 
 //db
 
